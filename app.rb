9364648b9d7cb2047d54a4525e93f6c2a5766165
fquilenan <fquilenan@gmail.com>
2020-03-15 18:27:04 +0000

    Sunday morning

--- app.rb
+++ app.rb
@@ -19,6 +19,11 @@ after { puts; }                                                                 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
 users_table = DB.from(:users)
+account_sid = "AC464bc6fa90c95e37140f6043e9eef3ec"
+auth_token = "cba8fdcaa26054cac3bdd648be2b0c1f"
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+
 
 before do
     @current_user = users_table.where(id: session["user_id"]).to_a[0]
@@ -36,23 +41,36 @@ get "/events" do
     view "events"
 end
 
+get "/events/new" do
+    view "new_event"
+end
+post "/events/create" do
+    puts params
+    events_table.insert(title: params["title"], 
+                        description: params["description"], 
+                        date: params["date"], 
+                        location: params["location"])
+    view "create_event"
+end
+
+
+
 get "/events/:id" do
     @event = events_table.where(id: params[:id]).to_a[0]
     @rsvps = rsvps_table.where(event_id: @event[:id])
     @going_count = rsvps_table.where(event_id: @event[:id], going: true).count
     @users_table = users_table
     
-    # @lat = rand(-90.0..90.0)
-    # @long = rand(-180.0..180.0)
-   
-    
     results = Geocoder.search(@event[:location])
-    
     lat_lng = results.first.coordinates 
     @lat_long = "#{lat_lng[0]}, #{lat_lng[1]}" 
     
     view "event"
 end
+
+
+
+
 
 get "/events/:id/rsvps/new" do
     @event = events_table.where(id: params[:id]).to_a[0]
@@ -66,6 +84,16 @@ get "/events/:id/rsvps/create" do
                        user_id: session["user_id"],
                        going: params["going"],
                        comments: params["comments"])
+
+
+    title = @event[:title]
+    date = @event[:date]
+    location = @event[:location]
+    client.messages.create(
+        from: "+12029328591", 
+        to: "+12247306141",
+        body: "Your reservation for #{title} is now confirmed. Remember, on #{date} at #{location}, see you there!" 
+        )
     view "create_rsvp"
 end
 
@@ -77,6 +105,16 @@ post "/users/create" do
     puts params
     hashed_password = BCrypt::Password.create(params["password"])
     users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    
+    # Trying to personalize msg
+    #name = @users_table["name"]
+    #{name}
+
+    client.messages.create(
+        from: "+12029328591", 
+        to: "+12247306141",
+        body: "Thanks for signing up! Now you can explore all the events we have for you."
+        )
     view "create_user"
 end
 
@@ -101,5 +139,7 @@ get "/logout" do
     @current_user = nil
     view "logout"
 end
+
+
 
 
